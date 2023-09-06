ea614d943c832faca6db4d3f6e245703e7ddf878
poojaasri <144219341+poojaasri@users.noreply.github.com>
2023-09-06 22:26:46 +0530

    main(2).py

--- challenge unit - 1/main.py
+++ challenge unit - 1/main.py
@@ -2,6 +2,8 @@
 def is_leap_year(year):
   if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     return "Leap year"
+  elif year%100 == 0:
+    return "Not a leap year"
   else:
     return "Not a leap year"
 
